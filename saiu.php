fad4c49cb5d0d59cee0e9f3c4f8a86bf370bc7ac
Christian Torres <40393667+christiantorres5@users.noreply.github.com>
2020-08-03 14:52:59 -0300

    Delete saiu.php

--- saiu.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-unset($_SESSION['usuario'], $_SESSION['usuarioid']);
-
-$_SESSION['msg'] = "<center style='color:green';>Deslogado com Sucesso</center>";
-header("Location:login.php");
-
-?>
